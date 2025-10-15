7c67be24bffbdd0e91e90d1f365535efc203e37a
Stimpicus <56415687+Stimpicus@users.noreply.github.com>
2025-10-15 10:53:22 -0400

    Add files via upload

---
+++ Content/CharacterPalette_Version2.h
@@ -0,0 +1,646 @@
+	Start X	Start Y	Width	Height	Color
+Torso	0	0	64	320	
+Color 1	0	0	32	64	
+Shade	32	0	32	32	
+Highlight	32	32	32	32	
+Color 2	0	64	32	64	
+Shade	32	64	32	32	
+Highlight	32	96	32	32	
+Color 3	0	128	32	64	
+Shade	32	128	32	32	
+Highlight	32	160	32	32	
+Color 4	0	192	32	64	
+Shade	32	192	32	32	
+Highlight	32	224	32	32	
+Color 5	0	256	32	64	
+Shade	32	256	32	32	
+Highlight	32	288	32	32	
+Hips	0	320	64	320	
+Color 1	0	320	32	64	
+Shade	32	320	32	32	
+Highlight	32	352	32	32	
+Color 2	0	384	32	64	
+Shade	32	384	32	32	
+Highlight	32	416	32	32	
+Color 3	0	448	32	64	
+Shade	32	448	32	32	
+Highlight	32	480	32	32	
+Color 4	0	512	32	64	
+Shade	32	512	32	32	
+Highlight	32	544	32	32	
+Color 5	0	576	32	64	
+Shade	32	576	32	32	
+Highlight	32	608	32	32	
+Skin	0	640	64	192	
+Color 1	0	640	64	64	
+Color 2	0	704	64	64	
+Color 3	0	768	64	64	
+Scar	0	832	64	64	
+Lips	0	896	64	64	
+Arm Attire Left	64	0	64	320	
+Color 1	64	0	32	64	
+Shade	96	0	32	32	
+Highlight	96	32	32	32	
+Color 2	64	64	32	64	
+Shade	96	64	32	32	
+Highlight	96	96	32	32	
+Color 3	64	128	32	64	
+Shade	96	128	32	32	
+Highlight	96	160	32	32	
+Color 4	64	192	32	64	
+Shade	96	192	32	32	
+Highlight	96	224	32	32	
+Color 5	64	256	32	64	
+Shade	96	256	32	32	
+Highlight	96	288	32	32	
+Leg Left	64	320	64	320	
+Color 1	64	320	32	64	
+Shade	96	320	32	32	
+Highlight	96	352	32	32	
+Color 2	64	384	32	64	
+Shade	96	384	32	32	
+Highlight	96	416	32	32	
+Color 3	64	448	32	64	
+Shade	96	448	32	32	
+Highlight	96	480	32	32	
+Color 4	64	512	32	64	
+Shade	96	512	32	32	
+Highlight	96	544	32	32	
+Color 5	64	576	32	64	
+Shade	96	576	32	32	
+Highlight	96	608	32	32	
+Ear Left	64	640	64	64	
+Eyelid Right	64	704	64	64	
+Fingernail Left	64	768	64	64	
+Tonail Left	64	832	64	64	
+Nose	64	896	64	64	
+Arm Attire Right	128	0	64	320	
+Color 1	128	0	32	64	
+Shade	160	0	32	32	
+Highlight	160	32	32	32	
+Color 2	128	64	32	64	
+Shade	160	64	32	32	
+Highlight	160	96	32	32	
+Color 3	128	128	32	64	
+Shade	160	128	32	32	
+Highlight	160	160	32	32	
+Color 4	128	192	32	64	
+Shade	160	192	32	32	
+Highlight	160	224	32	32	
+Color 5	128	256	32	64	
+Shade	160	256	32	32	
+Highlight	160	288	32	32	
+Leg Right	128	320	64	320	
+Color 1	128	320	32	64	
+Shade	160	320	32	32	
+Highlight	160	352	32	32	
+Color 2	128	384	32	64	
+Shade	160	384	32	32	
+Highlight	160	416	32	32	
+Color 3	128	448	32	64	
+Shade	160	448	32	32	
+Highlight	160	480	32	32	
+Color 4	128	512	32	64	
+Shade	160	512	32	32	
+Highlight	160	544	32	32	
+Color 5	128	576	32	64	
+Shade	160	576	32	32	
+Highlight	160	608	32	32	
+Ear Right	128	640	64	64	
+Eyelid Left	128	704	64	64	
+Fingernail Right	128	768	64	64	
+Toenail Right	128	832	64	64	
+Mouth	128	896	64	64	
+Hand Attire Left	192	0	64	320	
+Color 1	192	0	32	64	
+Shade	224	0	32	32	
+Highlight	224	32	32	32	
+Color 2	192	64	32	64	
+Shade	224	64	32	32	
+Highlight	224	96	32	32	
+Color 3	192	128	32	64	
+Shade	224	128	32	32	
+Highlight	224	160	32	32	
+Color 4	192	192	32	64	
+Shade	224	192	32	32	
+Highlight	224	224	32	32	
+Color 5	192	256	32	64	
+Shade	224	256	32	32	
+Highlight	224	288	32	32	
+Foot Left	192	320	64	320	
+Color 1	192	320	32	64	
+Shade	224	320	32	32	
+Highlight	224	352	32	32	
+Color 2	192	384	32	64	
+Shade	224	384	32	32	
+Highlight	224	416	32	32	
+Color 3	192	448	32	64	
+Shade	224	448	32	32	
+Highlight	224	480	32	32	
+Color 4	192	512	32	64	
+Shade	224	512	32	32	
+Highlight	224	544	32	32	
+Color 5	192	576	32	64	
+Shade	224	576	32	32	
+Highlight	224	608	32	32	
+Teeth	192	640	64	128	
+Color 1	192	640	64	64	
+Color 2	192	704	64	64	
+Gums	192	768	64	64	
+Teeth Accessory	192	832	64	64	
+Tongue	192	896	64	64	
+Hand Attire Right	256	0	64	320	
+Color 1	256	0	32	64	
+Shade	288	0	32	32	
+Highlight	288	32	32	32	
+Color 2	256	64	32	64	
+Shade	288	64	32	32	
+Highlight	288	96	32	32	
+Color 3	256	128	32	64	
+Shade	288	128	32	32	
+Highlight	288	160	32	32	
+Color 4	256	192	32	64	
+Shade	288	192	32	32	
+Highlight	288	224	32	32	
+Color 5	256	256	32	64	
+Shade	288	256	32	32	
+Highlight	288	288	32	32	
+Foot Right	256	320	64	320	
+Color 1	256	320	32	64	
+Shade	288	320	32	32	
+Highlight	288	352	32	32	
+Color 2	256	384	32	64	
+Shade	288	384	32	32	
+Highlight	288	416	32	32	
+Color 3	256	448	32	64	
+Shade	288	448	32	32	
+Highlight	288	480	32	32	
+Color 4	256	512	32	64	
+Shade	288	512	32	32	
+Highlight	288	544	32	32	
+Color 5	256	576	32	64	
+Shade	288	576	32	32	
+Highlight	288	608	32	32	
+EyeOut Left	256	640	64	64	
+EyeEdge Left	256	704	64	64	
+EyeInner Left	256	768	64	64	
+EyeColor Left	256	832	64	64	
+EyeHighlight Left	256	896	64	64	
+Shoulder Attire Left	320	0	64	320	
+Color 1	320	0	32	64	
+Shade	352	0	32	32	
+Highlight	352	32	32	32	
+Color 2	320	64	32	64	
+Shade	352	64	32	32	
+Highlight	352	96	32	32	
+Color 3	320	128	32	64	
+Shade	352	128	32	32	
+Highlight	352	160	32	32	
+Color 4	320	192	32	64	
+Shade	352	192	32	32	
+Highlight	352	224	32	32	
+Color 5	320	256	32	64	
+Shade	352	256	32	32	
+Highlight	352	288	32	32	
+Head Attachment	320	320	64	320	
+Color 1	320	320	32	64	
+Shade	352	320	32	32	
+Highlight	352	352	32	32	
+Color 2	320	384	32	64	
+Shade	352	384	32	32	
+Highlight	352	416	32	32	
+Color 3	320	448	32	64	
+Shade	352	448	32	32	
+Highlight	352	480	32	32	
+Color 4	320	512	32	64	
+Shade	352	512	32	32	
+Highlight	352	544	32	32	
+Color 5	320	576	32	64	
+Shade	352	576	32	32	
+Highlight	352	608	32	32	
+EyeOut Right	320	640	64	64	
+EyeEdge Right	320	704	64	64	
+EyeInner Right	320	768	64	64	
+EyeColor Right	320	832	64	64	
+EyeHighlight Right	320	896	64	64	
+Shoulder Attire Right	384	0	64	320	
+Color 1	384	0	32	64	
+Shade	416	0	32	32	
+Highlight	416	32	32	32	
+Color 2	384	64	32	64	
+Shade	416	64	32	32	
+Highlight	416	96	32	32	
+Color 3	384	128	32	64	
+Shade	416	128	32	32	
+Highlight	416	160	32	32	
+Color 4	384	192	32	64	
+Shade	416	192	32	32	
+Highlight	416	224	32	32	
+Color 5	384	256	32	64	
+Shade	416	256	32	32	
+Highlight	416	288	32	32	
+Face Attachment	384	320	64	320	
+Color 1	384	320	32	64	
+Shade	416	320	32	32	
+Highlight	416	352	32	32	
+Color 2	384	384	32	64	
+Shade	416	384	32	32	
+Highlight	416	416	32	32	
+Color 3	384	448	32	64	
+Shade	416	448	32	32	
+Highlight	416	480	32	32	
+Color 4	384	512	32	64	
+Shade	416	512	32	32	
+Highlight	416	544	32	32	
+Color 5	384	576	32	64	
+Shade	416	576	32	32	
+Highlight	416	608	32	32	
+Brow Left	384	640	64	64	
+Hair	384	704	64	128	
+Color 1	384	704	64	64	
+Color 2	384	768	64	64	
+Hair Stubble	384	832	64	64	
+Hair Accessory	384	896	64	64	
+Elbow Attire Left	448	0	64	320	
+Color 1	448	0	32	64	
+Shade	480	0	32	32	
+Highlight	480	32	32	32	
+Color 2	448	64	32	64	
+Shade	480	64	32	32	
+Highlight	480	96	32	32	
+Color 3	448	128	32	64	
+Shade	480	128	32	32	
+Highlight	480	160	32	32	
+Color 4	448	192	32	64	
+Shade	480	192	32	32	
+Highlight	480	224	32	32	
+Color 5	448	256	32	64	
+Shade	480	256	32	32	
+Highlight	480	288	32	32	
+Back Attachment	448	320	64	320	
+Color 1	448	320	32	64	
+Shade	480	320	32	32	
+Highlight	480	352	32	32	
+Color 2	448	384	32	64	
+Shade	480	384	32	32	
+Highlight	480	416	32	32	
+Color 3	448	448	32	64	
+Shade	480	448	32	32	
+Highlight	480	480	32	32	
+Color 4	448	512	32	64	
+Shade	480	512	32	32	
+Highlight	480	544	32	32	
+Color 5	448	576	32	64	
+Shade	480	576	32	32	
+Highlight	480	608	32	32	
+Brow Right	448	640	64	64	
+Facial Hair	448	704	64	128	
+Color 1	448	704	64	64	
+Color 2	448	768	64	64	
+Facial Hair Stubble	448	832	64	64	
+Facial Hair Accessory	448	896	64	64	
+Elbow Attire Right	512	0	64	320	
+Color 1	512	0	32	64	
+Shade	544	0	32	32	
+Highlight	544	32	32	32	
+Color 2	512	64	32	64	
+Shade	544	64	32	32	
+Highlight	544	96	32	32	
+Color 3	512	128	32	64	
+Shade	544	128	32	32	
+Highlight	544	160	32	32	
+Color 4	512	192	32	64	
+Shade	544	192	32	32	
+Highlight	544	224	32	32	
+Color 5	512	256	32	64	
+Shade	544	256	32	32	
+Highlight	544	288	32	32	
+Metals	512	320	64	256	
+Color 1	512	320	32	64	
+Shade	544	320	32	32	
+Highlight	544	352	32	32	
+Color 2	512	384	32	64	
+Shade	544	384	32	32	
+Highlight	512	448	32	32	
+Color 3	512	448	32	64	
+Shade	544	448	32	32	
+Highlight	544	480	32	32	
+Color 4	512	512	32	64	
+Shade	544	512	32	32	
+Highlight	544	544	32	32	
+Jewelry Metal	512	576	64	128	
+Color 1	512	576	32	64	
+Shade	544	576	32	32	
+Highlight	544	608	32	32	
+Color 2	512	640	32	64	
+Shade	544	640	32	32	
+Highlight	512	704	32	32	
+Glass	512	704	64	128	
+Color 1	512	704	32	64	
+Shade	544	704	32	32	
+Highlight	544	736	32	32	
+Color 2	512	768	32	64	
+Shade	544	768	32	32	
+Highlight	512	832	32	32	
+Glow	512	832	64	128	
+Color 1	512	832	32	64	
+Tint	544	832	32	64	
+Color 2	512	896	32	64	
+Tint	544	896	32	64	
+Knee Attire Left	576	0	64	320	
+Color 1	576	0	32	64	
+Shade	608	0	32	32	
+Highlight	608	32	32	32	
+Color 2	576	64	32	64	
+Shade	608	64	32	32	
+Highlight	576	128	32	32	
+Color 3	576	128	32	64	
+Shade	608	128	32	32	
+Highlight	608	160	32	32	
+Color 4	576	192	32	64	
+Shade	608	192	32	32	
+Highlight	608	224	32	32	
+Color 5	576	256	32	64	
+Shade	608	256	32	32	
+Highlight	608	288	32	32	
+Transparent	576	320	64	128	
+Color 1	576	320	32	64	
+Shade	608	320	32	32	
+Highlight	608	352	32	32	
+Color 2	576	384	32	64	
+Shade	608	384	32	32	
+Highlight	576	448	32	32	
+Digital Screen	576	448	64	128	
+Color 1	576	448	32	64	
+Shade	608	448	32	32	
+Highlight	608	480	32	32	
+Color 2	576	512	32	64	
+Shade	608	512	32	32	
+Highlight	576	576	32	32	
+Rubber	576	576	64	128	
+Color 1	576	576	32	64	
+Shade	608	576	32	32	
+Highlight	608	608	32	32	
+Color 2	576	640	32	64	
+Shade	608	640	32	32	
+Highlight	576	704	32	32	
+Plastic	576	704	64	128	
+Color 1	576	704	32	64	
+Shade	608	704	32	32	
+Highlight	608	736	32	32	
+Color 2	576	768	32	64	
+Shade	608	768	32	32	
+Highlight	576	832	32	32	
+Bone	576	832	64	128	
+Color 1	576	832	32	64	
+Shade	608	832	32	32	
+Highlight	608	864	32	32	
+Color 2	576	896	32	64	
+Shade	608	896	32	32	
+Highlight	576	960	32	32	
+Knee Attire Right	640	0	64	320	
+Color 1	640	0	32	64	
+Shade	672	0	32	32	
+Highlight	672	32	32	32	
+Color 2	640	64	32	64	
+Shade	672	64	32	32	
+Highlight	672	96	32	32	
+Color 3	640	128	32	64	
+Shade	672	128	32	32	
+Highlight	672	160	32	32	
+Color 4	640	192	32	64	
+Shade	672	192	32	32	
+Highlight	672	224	32	32	
+Color 5	640	256	32	64	
+Shade	672	256	32	32	
+Highlight	672	288	32	32	
+Fur	640	320	64	128	
+Color 1	640	320	32	64	
+Shade	672	320	32	32	
+Highlight	672	352	32	32	
+Color 2	640	384	32	64	
+Shade	672	384	32	32	
+Highlight	672	416	32	32	
+Rope	640	448	64	128	
+Color 1	640	448	32	64	
+Shade	672	448	32	32	
+Highlight	672	480	32	32	
+Color 2	640	512	32	64	
+Shade	672	512	32	32	
+Highlight	672	544	32	32	
+Strap	640	576	64	128	
+Color 1	640	576	32	64	
+Shade	672	576	32	32	
+Highlight	672	608	32	32	
+Color 2	640	640	32	64	
+Shade	672	640	32	32	
+Highlight	672	672	32	32	
+Stitch	640	704	64	128	
+Color 1	640	704	32	64	
+Shade	672	704	32	32	
+Highlight	672	736	32	32	
+Color 2	640	768	32	64	
+Shade	672	768	32	32	
+Highlight	672	800	32	32	
+Bandage	640	832	64	128	
+Color 1	640	832	32	64	
+Shade	672	832	32	32	
+Highlight	672	864	32	32	
+Color 2	640	896	32	64	
+Shade	672	896	32	32	
+Highlight	672	928	32	32	
+Hip Front Attachment	704	0	64	320	
+Color 1	704	0	32	64	
+Shade	736	0	32	32	
+Highlight	736	32	32	32	
+Color 2	704	64	32	64	
+Shade	736	64	32	32	
+Highlight	736	96	32	32	
+Color 3	704	128	32	64	
+Shade	736	128	32	32	
+Highlight	736	160	32	32	
+Color 4	704	192	32	64	
+Shade	736	192	32	32	
+Highlight	736	224	32	32	
+Color 5	704	256	32	64	
+Shade	736	256	32	32	
+Highlight	736	288	32	32	
+Paper	704	320	64	128	
+Color 1	704	320	32	64	
+Shade	736	320	32	32	
+Highlight	736	352	32	32	
+Color 2	704	384	32	64	
+Shade	736	384	32	32	
+Highlight	736	416	32	32	
+Wood	704	448	64	128	
+Color 1	704	448	32	64	
+Shade	736	448	32	32	
+Highlight	736	480	32	32	
+Color 2	704	512	32	64	
+Shade	736	512	32	32	
+Highlight	736	544	32	32	
+Horns	704	576	64	128	
+Color 1	704	576	32	64	
+Shade	736	576	32	32	
+Highlight	736	608	32	32	
+Color 2	704	640	32	64	
+Shade	736	640	32	32	
+Highlight	736	672	32	32	
+Feathers	704	704	64	128	
+Color 1	704	704	32	64	
+Shade	736	704	32	32	
+Highlight	736	736	32	32	
+Color 2	704	768	32	64	
+Shade	736	768	32	32	
+Highlight	736	800	32	32	
+Jewelry Gem	704	832	64	128	
+Color 1	704	832	32	64	
+Shade	736	832	32	32	
+Highlight	736	864	32	32	
+Color 2	704	896	32	64	
+Shade	736	896	32	32	
+Highlight	736	928	32	32	
+Hip Left Attachment	768	0	64	320	
+Color 1	768	0	32	64	
+Shade	800	0	32	32	
+Highlight	800	32	32	32	
+Color 2	768	64	32	64	
+Shade	800	64	32	32	
+Highlight	800	96	32	32	
+Color 3	768	128	32	64	
+Shade	800	128	32	32	
+Highlight	800	160	32	32	
+Color 4	768	192	32	64	
+Shade	800	192	32	32	
+Highlight	800	224	32	32	
+Color 5	768	256	32	64	
+Shade	800	256	32	32	
+Highlight	800	288	32	32	
+Flesh	768	320	64	128	
+Color 1	768	320	32	64	
+Shade	800	320	32	32	
+Highlight	800	352	32	32	
+Color 2	768	384	32	64	
+Shade	800	384	32	32	
+Highlight	800	416	32	32	
+Concrete	768	448	64	128	
+Color 1	768	448	32	64	
+Shade	800	448	32	32	
+Highlight	800	480	32	32	
+Color 2	768	512	32	64	
+Shade	800	512	32	32	
+Highlight	800	544	32	32	
+Hip Right Attachment	832	0	64	320	
+Color 1	832	0	32	64	
+Shade	864	0	32	32	
+Highlight	864	32	32	32	
+Color 2	832	64	32	64	
+Shade	864	64	32	32	
+Highlight	864	96	32	32	
+Color 3	832	128	32	64	
+Shade	864	128	32	32	
+Highlight	864	160	32	32	
+Color 4	832	192	32	64	
+Shade	864	192	32	32	
+Highlight	864	224	32	32	
+Color 5	832	256	32	64	
+Shade	864	256	32	32	
+Highlight	864	288	32	32	
+Hip Back Attachment	896	0	64	320	
+Color 1	896	0	32	64	
+Shade	928	0	32	32	
+Highlight	928	32	32	32	
+Color 2	896	64	32	64	
+Shade	928	64	32	32	
+Highlight	928	96	32	32	
+Color 3	896	128	32	64	
+Shade	928	128	32	32	
+Highlight	928	160	32	32	
+Color 4	896	192	32	64	
+Shade	928	192	32	32	
+Highlight	928	224	32	32	
+Color 5	896	256	32	64	
+Shade	928	256	32	32	
+Highlight	928	288	32	32	
+Fire	896	320	64	128	
+Color 1	896	320	32	64	
+Shade	928	320	32	32	
+Highlight	928	352	32	32	
+Color 2	896	384	32	64	
+Shade	928	384	32	32	
+Highlight	928	416	32	32	
+Lava	896	448	64	128	
+Color 1	896	448	32	64	
+Shade	928	448	32	32	
+Highlight	928	480	32	32	
+Color 2	896	512	32	64	
+Shade	928	512	32	32	
+Highlight	928	544	32	32	
+Vegetation	896	576	64	128	
+Color 1	896	576	32	64	
+Shade	928	576	32	32	
+Highlight	928	608	32	32	
+Color 2	896	640	32	64	
+Shade	928	640	32	32	
+Highlight	928	672	32	32	
+Water	896	704	64	128	
+Color 1	896	704	32	64	
+Shade	928	704	32	32	
+Highlight	928	736	32	32	
+Color 2	896	768	32	64	
+Shade	928	768	32	32	
+Highlight	928	800	32	32	
+Ice	896	832	64	128	
+Color 1	896	832	32	64	
+Shade	928	832	32	32	
+Highlight	928	864	32	32	
+Color 2	896	896	32	64	
+Shade	928	896	32	32	
+Highlight	928	928	32	32	
+Reference Palette	960	0	64	960	
+Color 1	960	0	32	64	
+Shade	992	0	32	32	
+Highlight	992	32	32	32	
+Color 2	960	64	32	64	
+Shade	992	64	32	32	
+Highlight	992	96	32	32	
+Color 3	960	128	32	64	
+Shade	992	128	32	32	
+Highlight	992	160	32	32	
+Color 4	960	192	32	64	
+Shade	992	192	32	32	
+Highlight	992	224	32	32	
+Color 5	960	256	32	64	
+Shade	992	256	32	32	
+Highlight	992	288	32	32	
+Color 6	960	320	32	64	
+Shade	992	320	32	32	
+Highlight	992	352	32	32	
+Color 7	960	384	32	64	
+Shade	992	384	32	32	
+Highlight	992	416	32	32	
+Color 8	960	448	32	64	
+Shade	992	448	32	32	
+Highlight	992	480	32	32	
+Color 9	960	512	32	64	
+Shade	992	512	32	32	
+Highlight	992	544	32	32	
+Color 10	960	576	32	64	
+Shade	992	576	32	32	
+Highlight	992	608	32	32	
+Color 11	960	640	32	64	
+Shade	992	640	32	32	
+Highlight	992	672	32	32	
+Color 12	960	704	32	64	
+Shade	992	704	32	32	
+Highlight	992	736	32	32	
+Color 13	960	768	32	64	
+Shade	992	768	32	32	
+Highlight	992	800	32	32	
+Color 14	960	832	32	64	
+Shade	992	832	32	32	
+Highlight	992	864	32	32	
+Color 15	960	896	32	64	
+Shade	992	896	32	32	
+Highlight	992	928	32	32	
+Cuts	960	960	64	64	
